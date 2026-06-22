600a3327165049296e1c69b93eaeb619070eb41a
Evan Burnett <juicyseals@gmail.com>
2026-06-22 00:32:25 -0400

    Add contextmanager and resilient get_session to DB session helper

--- db/session.py
+++ db/session.py
@@ -1,6 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, DeclarativeBase
 from dotenv import load_dotenv
+from contextlib import contextmanager
 import os
 
 load_dotenv(os.path.join(os.path.dirname(__file__), '..', '.env'))
@@ -12,9 +13,13 @@ engine = create_engine(os.getenv('DATABASE_URL'))
 
 SessionLocal = sessionmaker(bind=engine)
 
+@contextmanager
 def get_session():
     session = SessionLocal()
     try:
         yield session
+    except Exception:
+        session.rollback()
+        raise
     finally:
         session.close()
